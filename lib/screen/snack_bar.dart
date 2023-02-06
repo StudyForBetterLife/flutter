e668dbeb49724e06584cf5bf534d8e29a5954c5b
hjoon <guswns3371@gmail.com>
2023-02-06 22:31:51 +0900

    토스트 메세지(Toast message)
    - `fluttertoast: ^8.0.8`

--- lib/screen/snack_bar.dart
+++ lib/screen/snack_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class MySnackBar extends StatelessWidget {
   const MySnackBar({super.key});
@@ -12,20 +13,46 @@ class MySnackBar extends StatelessWidget {
       ),
       body: Builder(
           builder: (ctx) => Center(
-                child: TextButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(Color.alphaBlend(Colors.black, Colors.black))
-                  ),
-                  onPressed: () {
-                    ScaffoldMessenger.of(ctx)
-                        .showSnackBar(const SnackBar(content: Text("hello")));
-                  },
-                  child: const Text(
-                    "show me",
-                    style: TextStyle(color: Colors.white),
-                  ),
+                child: Column(
+                  children: [
+                    TextButton(
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(
+                              Color.alphaBlend(Colors.black, Colors.black))),
+                      onPressed: () {
+                        ScaffoldMessenger.of(ctx).showSnackBar(const SnackBar(
+                            content: Text(
+                          "hello",
+                          textAlign: TextAlign.center,
+                        )));
+                      },
+                      child: const Text(
+                        "show me",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          flutterToast();
+                        },
+                        child: const Text(
+                          "Toast",
+                          style: TextStyle(color: Colors.blue),
+                        ))
+                  ],
                 ),
               )),
     );
   }
+}
+
+void flutterToast() {
+  Fluttertoast.showToast(
+      msg: "Flutter",
+      gravity: ToastGravity.BOTTOM,
+      webPosition: "center",
+      backgroundColor: Colors.redAccent,
+      fontSize: 20.0,
+      textColor: Colors.white,
+      toastLength: Toast.LENGTH_SHORT);
 }
